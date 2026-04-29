d3108d84fa55a99e464235f145d28066ed8d2002
jungshell115 <jungshell@gmail.com>
2026-04-29 13:29:47 +0900

    Update wsgi.py

--- kpi_project/wsgi.py
+++ kpi_project/wsgi.py
@@ -4,6 +4,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kpi_project.settings')
 
 application = get_wsgi_application()
-
-# Vercel app alias
 app = application
+
+# Vercel 환경 데이터베이스 세팅 및 정적 파일 자동화
+try:
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'migrate'])
+    execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+except Exception as e:
+    print(f"Startup scripts failed: {e}")
